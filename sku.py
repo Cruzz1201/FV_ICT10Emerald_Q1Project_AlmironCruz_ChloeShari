a1f7186d52bb1fb973156f083c84c7f041e71316
Cchloric-Almiron <almiron.ca@obmontessori.edu.ph>
2026-09-20 20:46:40 +0800

    Add SKU generator function in sku.py
    Implement SKU generator function to create SKUs based on category, products, and stock.

---
+++ sku.py
@@ -0,0 +1,25 @@
+#Sku Generator 
+from pyscript import document, display 
+
+
+def sku_generator(e):
+    document.getElementById("results").innerHTML = "" #clear
+
+    #get the values from the input field
+    category = str(document.getElementById("category").value)  
+    products = str(document.getElementById("products").value) 
+    stock = str(document.getElementById("stock").value) 
+
+    #Use the first three letters
+    category_code = category[0:3].upper()
+    products_code = products[0:3].upper()
+
+    #generate the SKU
+    FinalSKU = category_code + "-" + products_code + "-" + stock
+
+    #display field 
+    display(f'SKU: {FinalSKU}', target="results")
+
+
+     
+
